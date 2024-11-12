01f7229ff6294a92332472a24d47e138a9a480c0
James Twin <james-twin@live.nmit.ac.nz>
2024-11-12 23:23:56 +1300

    firebase and javascript

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'freeCodeCamp_Firebase',
+  location: 'us-central1'
+};
+
--- index.js
+++ index.js
@@ -1,20 +1,85 @@
-import { initializeApp } from "https";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push, onValue, remove } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCeqWNXLLOTWBai10-jpoS10qszlX3NIec",
+  authDomain: "freecodecamp-project-2f8ff.firebaseapp.com",
+  databaseURL: "https://freecodecamp-project-2f8ff-default-rtdb.firebaseio.com",
+  projectId: "freecodecamp-project-2f8ff",
+  storageBucket: "freecodecamp-project-2f8ff.firebasestorage.app",
+  messagingSenderId: "234818821570",
+  appId: "1:234818821570:web:69dfd60a0ccc5825ea4a5b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
 
-const appSettings = {
-    databaseURL: "https://freecodecamp-project-2f8ff-default-rtdb.firebaseio.com/"
-}
 
 
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
-
-
-
-
-const prntToConsole = () => {
+const shoppingListEl = document.getElementById("shopping-list");
+const addItem = () => {
     let inputValue = inputFieldEl.value;
-    console.log(inputValue);
+    if (inputValue !== "") {
+        push(shoppingListInDB, inputValue);
+        clearInputFieldEl();
+    }
 }
-addButtonEl.addEventListener("click", prntToConsole);
+addButtonEl.addEventListener("click", addItem);
+
+const addToCartOnEnter = (action) => {
+    if (action.key === "Enter") {
+        addButtonEl.click();
+    }
+};
+
+
+
+inputFieldEl.addEventListener("keydown", addToCartOnEnter);
+
+onValue(shoppingListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val());
+        clearItemShoppingListEl();
+        for(let item of itemsArray) {
+            let currentItem = item;
+            let currentItemID = currentItem[0];
+            let currentItemValue = currentItem[1];
+            appendItemShoppingListEl(currentItem)
+        }
+    } else {
+        // This block will run when there are no items left
+        clearItemShoppingListEl();
+    }
+})
+
+
+function clearItemShoppingListEl() {
+    shoppingListEl.innerHTML = "";
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = "";
+}
+
+function appendItemShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    let newEl = document.createElement("li")
+    newEl.textContent = itemValue
+    newEl.addEventListener("dblclick", function () {
+            let locationItemToRemove = ref(database, `shoppingList/${itemID}`)
+            remove(locationItemToRemove)
+    })
+    shoppingListEl.append(newEl)
+}
+
